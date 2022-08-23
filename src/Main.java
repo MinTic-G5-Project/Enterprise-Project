2db9be90253e335b430c74b6abc2a2e0f60d5d0c
santiago morales <santiagomoraless2001@gmail.com>
2022-08-23 11:10:59 -0500

    add class movimiento dinero  and call class in class main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is a test");
-
+        LocalDate todaysDate = LocalDate.now();
+        //instancias
         Empresa minTic_G5=new Empresa();
-        minTic_G5.setDireccion("Cra 4 122-78");
+        Empleado emp =  new Empleado();
+        movimientoDinero mov = new movimientoDinero();
+
+        //set values class empresa
         minTic_G5.setNombre("Desarrolladores G5");
+        minTic_G5.setDireccion("Cra 4 122-78");
         minTic_G5.setTelefono("3173797793");
         minTic_G5.setNit("9005644-1");
 
+        //set values class empleado
+        emp.setNombre("juan");
+        emp.setCorreo("juan@decsg5.com");
+        emp.setRol("dev junior");
+        emp.setEmpresaPertenece("Desarolladores G5");
+
+
+        //set values class moviento dinero
+        mov.setMovimientoMonto(100.000);
+        mov.setMontonNegativo(10.000);
+        mov.setMontoPositivo(30.000);
+
+        System.out.println("***BIENVENIDO A "+ minTic_G5.getNombre()+"***");
+        System.out.println("EMPRESA : ");
         System.out.println("La empresa "+ minTic_G5.getNombre() + " ubicada en la ciudad de Medellin, con NIT "
                             + minTic_G5.getNit() + "se dedica a la produccion de software de calidad");
+
+        System.out.println("EMPLEADO :");
+        System.out.println("el empleado"+emp.getNombre()+" con el correo"+emp.getCorreo() + " en el cargo"+emp.getRol()
+        + " de la empresa" + emp.getEmpresaPertenece());
+
+        System.out.println("MOVIMIENTOS :");
+        System.out.println("El movimiento realizado" +todaysDate + "con un valor de " + mov.getMontonNegativo() +
+                "\n" + " para un saldo toltal de " +  mov.getMovimientoMonto());
+
+        System.out.println("El giro relizado el día" + todaysDate + " con un valor de "+ mov.getMontonNegativo());
+        System.out.println("Pago nomima " + todaysDate + " valor de " + mov.getMontoPositivo());
+
 
     }
 }
--- src/movimientoDinero.java
+++ src/movimientoDinero.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class movimientoDinero {
+public class movimientoDinero {
+    private double movimientoMonto,
+    montoPositivo,
+    montonNegativo,
+    Movimiento;
+
+    public movimientoDinero(double movimientoMonto, double montoPositivo, double montonNegativo, double movimiento) {
+        this.movimientoMonto = movimientoMonto;
+        this.montoPositivo = montoPositivo;
+        this.montonNegativo = montonNegativo;
+        Movimiento = movimiento;
+    }
+
+
+    public movimientoDinero() {
+    }
+
+    public double getMovimientoMonto() {
+        return movimientoMonto;
+    }
+
+    public void setMovimientoMonto(double movimientoMonto) {
+        this.movimientoMonto = movimientoMonto;
+    }
+
+    public double getMontoPositivo() {
+        return montoPositivo;
+    }
+
+    public void setMontoPositivo(double montoPositivo) {
+        this.montoPositivo = montoPositivo;
+    }
+
+    public double getMontonNegativo() {
+        return montonNegativo;
+    }
+
+    public void setMontonNegativo(double montonNegativo) {
+        this.montonNegativo = montonNegativo;
+    }
+
+    public double getMovimiento() {
+        return Movimiento;
+    }
+
+    public void setMovimiento(double movimiento) {
+        Movimiento = movimiento;
+    }
 }
